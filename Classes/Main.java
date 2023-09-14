fb7161314c4b0fe6f9b3b8c397c7a1d546ad7d32
ClaudioRibeiro2 <claudio_ribeiro2@estudante.sc.senai.br>
2023-09-14 14:17:45 -0300

    Changing the main method to the class Main

---
+++ Classes/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args){
+        Elemento elemento1 = new Elemento(2.2);
+        System.out.println(elemento1);
+    }
+}
--- Classes/Menu.java
+++ Classes/Menu.java
@@ -1,6 +1,3 @@
 public class Menu {
-     public static void main (String[] args){
-        Elemento elemento1 = new Elemento(2.2);
-        System.out.println(elemento1);
-     }
+
 }

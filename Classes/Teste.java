999150df492d9fce1408ddcffc360ec4890cb289
natskoe <koehlernathy@gmail.com>
2023-10-17 20:45:14 -0300

    Merge metodo

--- Classes/Teste.java
+++
@@ -1,6 +0,0 @@
-public class Teste {
-    public static void main(String[] args) {
-        Batalha batalha = new Batalha(new Criatura(), new Criatura());
-        batalha.iniciarBatalha();
-    }
-}
